b85fda0a65f460c39f93708fb1ce26f99f11edf1
Soran Nakagawa <s0ran.n.11921@gmail.com>
2019-07-01 19:38:12 +0900

    タップ操作の導入 別のタイルの指示

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, Mathf.Infinity))
+            {
+                hit.collider.gameObject.GetComponent<TileContraller>().Awake
+                (hit.collider.gameObject.GetComponent<TileContraller>().Line,
+                    hit.collider.gameObject.GetComponent<TileContraller>().Raw);
+                //hit.collider.gameObject.GetComponent<TileContraller>().ChangeColor();
+            }
+        }
+    }
+}
--- Assets/TileContraller.cs
+++ Assets/TileContraller.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TileContraller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Line;
+    public int Raw;
+
     void Start()
     {
         GetComponent<Renderer>().material.color = Color.white;
+        Line = (int)(2.5f-transform.position.z);
+        Raw = (int)(2.5f+transform.position.x);
+        Debug.Log(this.Line);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Awake(int Line,int Raw)
+    {
+        Debug.Log(this.Line);
+        Debug.Log(string.Format("Tile{0}-{1}", Line + 1, Raw));
+        GameObject next = GameObject.Find(string.Format("Tile{0}-{1}", Line+1, Raw));
+        next.GetComponent<TileContraller>().ChangeColor();
+    }
+
+    public void ChangeColor()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
     }
 }
+
+
